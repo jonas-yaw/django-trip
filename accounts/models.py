3cb600a48187ec73040f8938177da6a76b86d6d5
kk_jonas <jonasyawjnr@gmail.com>
2020-07-15 21:45:35 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        verbose_name="email address", max_length=255, unique=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
