b3e21728a86573c555eb5c3c878b4a633cd7318e
kk_jonas <jonasyawjnr@gmail.com>
2020-07-23 01:43:05 +0100

    heroku configurations added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dgango_trip_project.wsgi
--- dgango_trip_project/settings.py
+++ dgango_trip_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,12 +42,14 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -116,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 LOGIN_REDIRECT_URL = "home"
@@ -125,3 +129,4 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/upload_images")
 MEDIA_URL = "/media/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+django_heroku.settings(locals())
