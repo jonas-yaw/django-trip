e7545926f7977174beee33ab578f51cda8106f66
kk_jonas <jonasyawjnr@gmail.com>
2020-07-23 01:45:33 +0100

    changes made to settings

--- dgango_trip_project/settings.py
+++ dgango_trip_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "vpvx)^w(4uz9g5y+6ag409zy$5+gbkzoe++h0&qt2cwj4*)%qe"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 LOGIN_REDIRECT_URL = "home"
